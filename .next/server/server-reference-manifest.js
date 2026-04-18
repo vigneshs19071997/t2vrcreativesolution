21650635851b6678659ca51745aaedeccdd0543a
vignesh <vigneshs19071997@gmail.com>
2026-04-19 01:13:53 +0530

    Remove node_modules and updated next version

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"URRV8C1Az0+mszgzf3G0Yizv7PtBX5a23Mn97dStJdA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"myahvpFVd5hUQoIGqDKUpOidke0OUMSxSK1rp//eRZY=\"}"
